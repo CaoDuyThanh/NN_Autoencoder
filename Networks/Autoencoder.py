9bbf4ec4f925cb0f09c0c40e9c1eff835423572c
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-26 20:33:14 +0700

    Modified:
    - View result after train the model

--- Networks/Autoencoder.py
+++ Networks/Autoencoder.py
@@ -1,19 +1,25 @@
 from __future__ import print_function
 import timeit
+import Image
+import pickle
+import cPickle
 import Utils.DataHelper as DataHelper
 import Utils.CostFHelper as CostFHelper
 from pylab import *
 from Layers.HiddenLayer import *
+from Utils.FilterHelper import *
+
 
 # Hyper parameters
 DATASET_NAME = '../Dataset/mnist.pkl.gz'
-LEARNING_RATE = 0.005
-NUM_EPOCH = 500
-BATCH_SIZE = 20
+SAVE_PATH = '../Pretrained/model_corruption_30.pkl'
+LEARNING_RATE = 0.05
+NUM_EPOCH = 3000
+BATCH_SIZE = 50
 PATIENCE = 1000
 PATIENCE_INCREASE = 2
 IMPROVEMENT_THRESHOLD = 0.995
-VALIDATION_FREQUENCY = 500
+VALIDATION_FREQUENCY = 1000
 VISUALIZE_FREQUENCY = 5000
 
 ''' Load dataset | Create Model | Evaluate Model '''
@@ -55,7 +61,8 @@ def autoencoder():
         input = X,
         numIn = 28 * 28,
         numOut = 500,
-        activation = T.nnet.sigmoid
+        activation = T.nnet.sigmoid,
+        corruption = 0.3
     )
     hidLayer0Output = hidLayer0.Output()
     hidLayer0Params = hidLayer0.Params()
@@ -67,17 +74,18 @@ def autoencoder():
         input = hidLayer0Output,
         numIn = 500,
         numOut = 28 * 28,
-        activation=T.tanh,
+        activation=T.nnet.sigmoid,
         W = hidLayer0WTranspose
     )
     hidLayer1Output = hidLayer1.Output()
-    hidLayer1Params = hidLayer1.Params()
+    hidLayer1Params = [hidLayer1.b]
 
     # Evaluate model
-    costTrain = CostFHelper.MSE(hidLayer1Output, X)
+    costTrain = CostFHelper.BinaryCrossEntropy(hidLayer1Output, X)
 
     # List of params from model
-    params = hidLayer0Params
+    params = hidLayer0Params + \
+             hidLayer1Params
 
     # Define gradient
     grads = T.grad(costTrain, params)
@@ -91,7 +99,7 @@ def autoencoder():
     # Train model
     trainModel = theano.function(
         inputs = [Index],
-        outputs = costTrain,
+        outputs = [costTrain],
         updates = updates,
         givens = {
             X: trainSetX[Index * BATCH_SIZE : (Index + 1) * BATCH_SIZE]
@@ -105,10 +113,9 @@ def autoencoder():
         givens={
             X: trainSetX[Index: Index + 25]
         }
-
     )
 
-    error = CostFHelper.MSE(hidLayer1Output, X)
+    error = CostFHelper.BinaryCrossEntropy(hidLayer1Output, X)
     # Valid model
     validModel = theano.function(
         inputs = [Index],
@@ -126,6 +133,13 @@ def autoencoder():
             X: testSetX[Index * BATCH_SIZE: (Index + 1) * BATCH_SIZE]
         }
     )
+
+    # Load best model
+    file = open(SAVE_PATH)
+    hidLayer0.W.set_value(cPickle.load(file), borrow=True)
+    hidLayer0.b.set_value(cPickle.load(file), borrow=True)
+    hidLayer1.b.set_value(cPickle.load(file), borrow=True)
+    file.close()
 
     doneLooping = False
     iter = 0
@@ -147,7 +161,7 @@ def autoencoder():
                 for indexValidBatch in range(nValidBatchs):
                     err += validModel(indexValidBatch)
                 err /= nValidBatchs
-                print('Error = ', err)
+                print('Epoch = ', epoch, '| Error = ', err)
 
                 if (err < best_error):
                     if (err < best_error * IMPROVEMENT_THRESHOLD):
@@ -156,43 +170,39 @@ def autoencoder():
                     best_iter = iter
                     best_error = err
 
+                    # Save the model
+                    file = open(SAVE_PATH, 'wb')
+                    pickle.dump(hidLayer0.W.get_value(borrow = True), file, -1)
+                    pickle.dump(hidLayer0.b.get_value(borrow = True), file, -1)
+                    pickle.dump(hidLayer1.b.get_value(borrow = True), file, -1)
+                    file.close()
+
                     # Test on test set
                     test_losses = [testModel(i) for i in range(nTestBatchs)]
                     test_score = numpy.mean(test_losses)
-
-        if (patience < iter):
-            doneLooping = True
-            break
-
-
-    output = visualizeModel(0)
-    figure('Original image')
-    for k in range(5):
-        for l in range(5):
-            temp = trainSetX.get_value(borrow=True)[k * 5 + l].reshape((28, 28))
-            subplot(5, 5, k * 5 + l + 1)
-            gray()
-            axis('off')
-            imshow(temp)
-            draw()
-
-    figure('Decode image')
-    for k in range(5):
-        for l in range(5):
-            temp = output[0][k * 5 + l].reshape((28, 28))
-            subplot(5, 5, k * 5 + l + 1)
-            gray()
-            axis('off')
-            imshow(temp)
-            draw()
 
     end_time = timeit.default_timer()
     print(('Optimization complete. Best validation score of %f %% '
            'obtained at iteration %i, with test performance %f %%') %
           (best_error * 100., best_iter + 1, test_score * 100.))
     print(('The code for file ran for %.2fm' % ((end_time - start_time) / 60.)), file=sys.stderr)
+
+    # Load best model
+    file = open(SAVE_PATH)
+    hidLayer0.W.set_value(cPickle.load(file), borrow = True)
+    hidLayer0.b.set_value(cPickle.load(file), borrow = True)
+    hidLayer1.b.set_value(cPickle.load(file), borrow = True)
+    file.close()
+
+    # Show all fileters from model
+    image = Image.fromarray(tile_raster_images(
+        X=hidLayer0.W.get_value(borrow=True).T,
+        img_shape=(28, 28), tile_shape=(10, 10),
+        tile_spacing=(1, 1)))
+    image.save('filters_corruption_30.png')
+    imshow(image)
+    gray()
     show()
-    return 0
 
 
 if __name__ == '__main__':
